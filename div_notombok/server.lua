8b90f70931915f5a498dab628ac709f0f0c441ac
div <168165484+divyei@users.noreply.github.com>
2024-08-12 10:34:57 +0800

    Update server.lua

--- div_notombok/server.lua
+++ div_notombok/server.lua
@@ -1,3 +1,3 @@
-lib.callback.register('nakal:sungguh', function(source)
-    return DropPlayer(source, 'Nakal Sungguh Kamu Ya...') -- Kick Sudah mencukupi , Nak ban letak sendiri :)
+lib.callback.register('entah:la', function(source)
+    return DropPlayer(source, 'Nakal Sungguh Kamu Ya...')
 end)
