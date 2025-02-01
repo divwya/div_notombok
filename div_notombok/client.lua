0b3e44abe41e2ec7b7d92822cf7fc88f5a2ee8ca
div <168165484+divwya@users.noreply.github.com>
2025-02-01 13:37:42 +0800

    Update client.lua
    leh ker?

--- div_notombok/client.lua
+++ div_notombok/client.lua
@@ -1,34 +1,22 @@
-local leveltipu = 0 
-
-local function DisableTombok(monkey)
-    leveltipu = exports.xperience:GetRank()
-    CreateThread(function() 
-        while monkey do
-            if leveltipu >= 3 then break end
-            local ped = PlayerPedId()
-            if IsPedArmed(ped, 4) then
-                TriggerEvent("ox_inventory:disarm", true)
+local function cekLevel()
+    local lvl = exports.xperience:GetRank()
+    if lvl < 3 then
+        LocalPlayer.state.canUseWeapons = false
+        NetworkSetFriendlyFireOption(false)
+        CreateThread(function()
+            while true do
+                if lvl >= 3 then
+                    LocalPlayer.state.canUseWeapons = true 
+                    NetworkSetFriendlyFireOption(true)
+                    break
+                end
+                Wait(7000)
             end
-            DisableControlAction(0, 24, true) 
-            DisableControlAction(0, 25, true) 
-            DisableControlAction(0, 37, true) 
-            DisablePlayerFiring(ped, true)
-            DisableControlAction(0, 140, true)
-            if IsPedPerformingMeleeAction(ped) or IsPedShooting(ped) then
-                lib.callback.await('entah:la', false)
-            end
-            Wait(1) 
-        end
-    end)
-end
-
-lib.onCache('ped', function(value)
-    if value and LocalPlayer.state.isLoggedIn then 
-        leveltipu = exports.xperience:GetRank() 
+        end)
     end
-end)
+end
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function() 
     Wait(3000)
-    if exports.xperience:GetRank() < 3 then DisableTombok(true) end
+    cekLevel()
 end)
